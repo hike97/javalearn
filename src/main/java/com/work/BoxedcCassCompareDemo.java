331b689f5855ce28275ec528c3ce063ff5e3e201
luhy <seaatom@qq.com>
2022-04-21 09:49:11 +0800

    --20220421 1.ignore 模板更新 --close:

---
+++ src/main/java/com/work/BoxedcCassCompareDemo.java
@@ -0,0 +1,18 @@
+package com.work;
+
+/**
+ * @ClassName BoxedcCassCompareDemo
+ * @Description TODO
+ * @Author hike97
+ * @Date 2022/3/18 14:07
+ * @Version 1.0
+ **/
+public class BoxedcCassCompareDemo {
+    public static void main (String[] args) {
+        Byte bOne = 1;
+        Byte aOne = 1;
+        System.out.println (aOne.equals (bOne) );
+        int iOne = 1;
+        System.out.println (bOne == iOne);
+    }
+}
