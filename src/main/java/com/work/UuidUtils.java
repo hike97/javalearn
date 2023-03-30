23240ff6917abe498091659b62b6162c666ce7fc
luhy <luhy@rrxplatform.com>
2023-03-30 22:34:21 +0800

    20230330

---
+++ src/main/java/com/work/UuidUtils.java
@@ -0,0 +1,19 @@
+package com.work;
+
+import cn.hutool.core.util.IdUtil;
+import org.apache.commons.lang.math.RandomUtils;
+
+import java.util.UUID;
+
+/**
+ * @ClassName UuidUtils
+ * @Description TODO
+ * @Author hike97
+ * @Date 2022/11/25 10:56
+ * @Version 1.0
+ **/
+public class UuidUtils {
+    public static void main(String[] args) {
+        System.out.println(IdUtil.simpleUUID());
+    }
+}
